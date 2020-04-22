e5e7c7d66241871268b6919dd3114365b1f98b3b
marcbryan <marcboakyeflores98@gmail.com>
2020-04-22 16:45:35 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+// Variables entorno
+const PORT = process.env.PORT || 3000;
+var host = process.env.HOST;
+var db_name = process.env.DBNAME;
+
+var URI = 'mongodb+srv://'+host+'/'+db_name+'?retryWrites=true&w=majority';
+
+const mongoose = require('mongoose');
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: process.env.DBUSER,
+    pass: process.env.DBPASS
+});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function() {
+    console.log('Connected succesfully!!');
+    app.listen(PORT, function() {
+        console.log('Example app listening on port '+PORT);
+    });
+});
