83c3b6ac59c65e335371fc99842e179319b10a0b
marcbryan <marcboakyeflores98@gmail.com>
2020-04-27 21:35:50 +0200

    Login Users

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,11 +2,28 @@ var User = require('../models/User');
 
 /** Comprueba si los datos enviados por un usuario son válidos para hacer login (POST) */
 exports.user_login = function (req, res) {
-    // No implementado
-    res.send({msg: 'Hola '+req.body.user});
-    console.log(req);
-    console.log(req.body);
+    let email = req.body.email;
+    let pass = req.body.password;
+    // Log para ver en consola los datos
+    console.log('[INFO] Request at '+new Date()+' -> { email: "'+email+'", pass: "'+pass+'" }');
     
+    if (isset(email) && isset(pass)) {
+        User.findOne({email: email, password: pass}, '-_id -__v -credit_card', function(err, document) {
+            if (err) {
+                console.log(err);
+                res.send({status: 'ERROR', msg: 'Error de base de datos, intentalo más tarde.'});
+                return;
+            }
+            if (isset(document)) {
+                var user = new User(document);
+                res.send({status: 'OK', msg: 'Bienvenido '+user.name+'!', data: user.toJSON()});
+            } else {
+                res.send({status: 'ERROR', msg: 'E-Mail/Contraseña inválidos'});
+            }
+        });
+    } else {
+        res.send({status: 'ERROR', msg: 'E-Mail/Contraseña inválidos'});
+    }
 }
 
 /** Se encarga de crear un usuario por POST */
@@ -17,4 +34,12 @@ exports.user_create_post = function(req, res) {
 /** Se encarga de actualizar datos de un usuario por POST */
 exports.user_update_post = function(req, res) {
     res.send('UPDATE No implementado');
+}
+
+
+function isset(value) {
+    if (value !== undefined && value != null && value != '') {
+        return true;
+    }
+    return false;
 }
--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,13 @@
-const crypto = require('crypto');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const crypto = require('crypto');
+var moment = require('moment');
 
 var UserSchema = new Schema({
     name: {type: String, required: true},
     last_name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true, select: false},
     city: {type: String, required: true},
     zipcode: {type: String, required: true},
     phone: {type: String, required: true},
@@ -21,8 +22,21 @@ var UserSchema = new Schema({
     timestamps: { createdAt: true, updatedAt: false }
 });
 
+
 /**
- * Función para hashear texto
+ * TODO: Añadir comentario función
+ */
+UserSchema.methods.toJSON = function() {
+    var obj = this.toObject();
+    obj.createdAt = this.str_createdAt;
+    delete obj._id;
+    delete obj.password;
+    return obj;
+}
+   
+
+/**
+ * Función estática para hashear texto
  * @param {string} strPwd El texto a hashear
  * @return {string} El hash de strPwd
  */
@@ -30,10 +44,11 @@ UserSchema.statics.hashPassword = function(strPwd) {
     return crypto.createHash('sha256').update(strPwd).digest('hex');
 }
 
-UserSchema.virtual('url')
-          .get(function() {
-              return '/users/' + this._id;
-          });
+// TODO: Añadir comentario
+UserSchema.virtual('str_createdAt')
+    .get(function() {
+        return moment(this.createdAt).format('DD/MM/YYYY HH:mm:ss');
+    });
 
 // Exportar modelo
 module.exports = mongoose.model('User', UserSchema);
